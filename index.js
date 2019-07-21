68237b781ab0f900233aa3ffe675bbc64785f5d4
John Azzaro <jazzaro@temple.edu>
2019-07-21 10:19:49 -0700

    resolving server issue

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const mimeTypes = {                                                             
 }        
 
 http.createServer(function(req, res) {                                             // Then, you want to create a server
-  let uri = url.parse(req.url).pathname;                                               // use the url module and convert request.url into an object, and then get the path name and store as "uri"  
-  let fileName = path.join(process.cwd(), unescape(uri))                               // use the path module and join with two arguments: "process.cwd" returns string for current working directory and decondeURI to decode uri  
+	let uri = url.parse(req.url).pathname;                                               // use the url module and convert request.url into an object, and then get the path name and store as "uri"  
+  let fileName = path.join(process.cwd(), unescape(uri));                               // use the path module and join with two arguments: "process.cwd" returns string for current working directory and decondeURI to decode uri  
   console.log('Loading' + uri);                                                        // then log to console the uri begin loaded.
   let stats;
                                                                                    // Then to check for the file you are requesting (i.e. index.html, index.css, etc.) with a try/catch block
