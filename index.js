1c4ca5a74555b9a59b3f1c1408f1c02834e9b3c1
John Azzaro <jazzaro@temple.edu>
2019-07-21 10:39:57 -0700

    fixed html error (mimeType issue)

--- index.js
+++ index.js
@@ -13,23 +13,23 @@ const mimeTypes = {                                                             
 }        
 
 http.createServer(function(req, res) {                                             // Then, you want to create a server
-	let uri = url.parse(req.url).pathname;                                               // use the url module and convert request.url into an object, and then get the path name and store as "uri"  
+	let uri = url.parse(req.url).pathname;                                              // use the url module and convert request.url into an object, and then get the path name and store as "uri"  
   let fileName = path.join(process.cwd(), unescape(uri));                               // use the path module and join with two arguments: "process.cwd" returns string for current working directory and decondeURI to decode uri  
   console.log('Loading' + uri);                                                        // then log to console the uri begin loaded.
   let stats;
                                                                                    // Then to check for the file you are requesting (i.e. index.html, index.css, etc.) with a try/catch block
   try{                                                                                  
-    stats = fs.lstatSync(fileName);                                                     // return the status of the fileName (synchronously)
-  } catch(err) {                                                                       // if not, respond with the error
-    res.writeHead(404, {"Content-type":"text/plain"});                                 // ... by responding with an error status code and content type.
-    res.write('404 Not Found\n')                                                       // ... and write "404 Not found" and add \n to indicate end-of-line.
+    stats = fs.lstatSync(fileName);                                                    // return the status of the fileName (synchronously)
+  } catch(e) {                                                                       // if not, respond with the error
+    res.writeHead(404, {'Content-type': 'text/plain'});                                 // ... by responding with an error status code and content type.
+    res.write('404 Not Found\n');                                                       // ... and write "404 Not found" and add \n to indicate end-of-line.
     res.end();                                                                         // ... end response 
     return;                                                                            //... and return.
   }
 
   if(stats.isFile()) {                                                              // if it is a file...
-    let mimeTypes = mimeTypes[path.extname(fileName).split(".").reverse()[0]];         // find out what type of file it is
-    res.writeHead(200, {"Content-type": MimeType});                                    // response will be 200 (good) and content type
+    let mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];         // find out what type of file it is
+    res.writeHead(200, {"Content-type": mimeType});                                    // response will be 200 (good) and content type
 
     let fileStream = fs.createReadStream(fileName);                                    // for the filestream variable, it will be set to the file system module and chain a method called createReadStream
     fileStream.pipe(res);                                                              // then pipe the response
