119452e9fcaf364ca16144604fe516e82a2f18bc
John Azzaro <jazzaro@temple.edu>
2019-07-21 09:09:00 -0700

    mimeType object issue resolved

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const url = require('url');                                         // .., and u
 const path = require('path');                                       // ... and path module
 const fx = require('fs');                                           // ... and file system module
 
-                                                                    
+const mimeTypes = {                                                  // ... mimeTypes object ("mimeType" label used to identify a type of data) lists the types of data we can serve.
+  "html": "text/html",                                                  // so we want to be able to serve html
+  "js": "text/javascript",                                              // ... and javascript files 
+  "css": "text/css",                                                    // ... and css files         
+  "jpeg": "image/jpeg",                                                 // ... and jpeg images
+  "jpg": "image/jpg",                                                   // ... and jpg images
+  "png": "image/png",                                                   // ... and png images
+}        
+
