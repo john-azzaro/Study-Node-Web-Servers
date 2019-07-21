6428c3db78a6306ac502778e4269357dd80f44b4
John Azzaro <jazzaro@temple.edu>
2019-07-20 21:32:47 -0700

    added commentary to server code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const http = require('http');                                       // first, we load the http module and save as a constant "http"
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1';                                       // "hostname" is the local host
+const port = 3000;                                                  // "port" is the localhost we can use to access our application.
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const server = http.createServer((req, res) => {                   // with the http module, we want to create a server "createServer" method with...
+  res.statusCode = 200;                                            //... a reponse status code of 200 (means ok)
+  res.setHeader('Content-Type', 'text/plain');                     //... setting the content type to text
+  res.end('Hello World\n');                                        //... and end the response with "Hello World"
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, () => {                              // Then the server will listen on port 3000 (see "port" variable) and...
+  console.log(`Server running at http://${hostname}:${port}/`);    // ... in the console print out the hostname and port the server is on!
 });
